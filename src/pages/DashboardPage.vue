d81a0445b6590fff9f4ce97dbd437cf5bfe7455d
hakrithy <hakrithykim2020@gmail.com>
2025-09-02 10:19:08 +0700

    scaffold project

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,6 +1,46 @@
 <template>
     <div class="w-full h-full">
-        DASHBOARD
+        <div class="w-full h-full flex flex-col">
+
+            <!-- Header -->
+            <div class="header-box h-[25%] w-full flex justify-center items-center gap-2 py-2">
+                <div class="w-[25%] h-full content-one">
+                    <div class="w-full flex justify-center items-center h-full rounded-md bg-[#303030]">1</div>
+                </div>
+                <div class="w-[25%] h-full content-two">
+                    <div class="w-full flex justify-center items-center h-full rounded-md bg-[#303030]">2</div>
+                </div>
+                <div class="w-[25%] h-full content-three">
+                    <div class="w-full flex justify-center items-center h-full rounded-md bg-[#303030]">3</div>
+                </div>
+                <div class="w-[25%] h-full content-four">
+                    <div class="w-full flex justify-center items-center h-full rounded-md bg-[#303030]">4</div>
+                </div>
+            </div>
+
+            <!-- Center -->
+            <div class="center-box h-[50%] w-full">
+                <div class="w-full h-full content-one">
+                    <div class="w-full h-full flex justify-center items-center rounded-md bg-[#303030]">center</div>
+                </div>
+            </div>
+
+            <!-- Footer -->
+            <div class="footer-box h-[25%] w-full flex justify-center items-center gap-2 py-2">
+                <div class="w-[25%] h-full content-one">
+                    <div class="w-full flex justify-center items-center h-full rounded-md bg-[#303030]">1</div>
+                </div>
+                <div class="w-[25%] h-full content-two">
+                    <div class="w-full flex justify-center items-center h-full rounded-md bg-[#303030]">2</div>
+                </div>
+                <div class="w-[25%] h-full content-three">
+                    <div class="w-full flex justify-center items-center h-full rounded-md bg-[#303030]">3</div>
+                </div>
+                <div class="w-[25%] h-full content-four">
+                    <div class="w-full flex justify-center items-center h-full rounded-md bg-[#303030]">4</div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
